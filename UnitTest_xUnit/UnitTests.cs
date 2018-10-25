fdb131534ca4658a1ff23d565f3defda3c115362
Jose Beltran <bjabinn@hotmail.com>
2018-10-25 08:22:16 +0200

    commiting

---
+++ UnitTest_xUnit/UnitTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace UnitTest_xUnit
+{
+    public class UnitTests
+    {
+        //Test are always true in invariants conditions
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        //Test are always true for a particular set of data
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+
+    }
+}
